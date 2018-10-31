cec01183fc23a91252a012fd2142201f0268dfec
Alex Fihl <alex@fihl.net>
2018-10-31 15:23:34 +0100

    Added class separation for main and the logic in the "cpu"

---
+++ src/riscv_simulator/CPU.java
@@ -0,0 +1,87 @@
+package riscv_simulator;
+
+public class CPU {
+	
+	private int pc;
+	private int reg[] = new int[32];
+	private int program[];
+	
+	private int instruction;
+	private int opcode;
+	private int rd;
+	private int funt3;
+	private int rs1;
+	private int rs2;
+	private int funt7;
+	private int imm;
+	
+	public CPU() {
+		
+	}
+	
+	public int oneStep() {
+		instruction = program[pc];
+		opcode = instruction & 0x7f;
+		rd = (instruction >> 7) & 0x1f;
+		funt3 = (instruction >> 12) & 0x7;
+		rs1 = (instruction >> 15) & 0x1f;
+		rs2 = (instruction >> 20) & 0x1f;
+		funt7 = instruction >> 25;
+		imm = instruction >> 20;
+		
+		switch(opcode) {
+		case 0x13:
+			reg[rd] = reg[rs1] + imm;
+			break;
+		case 0x33:
+			switch(funt3) {
+			case 0x0:
+				if(funt7 == 0x00)
+					reg[rd] = reg[rs1] + reg[rs2]; //add
+				else
+					reg[rd] = reg[rs1] - reg[rs2]; //sub
+				break;
+			case 0x1:
+				reg[rd] = reg[rs1] << reg[rs2]; //sll
+				break;
+			case 0x2:
+				reg[rd] = reg[rs1] < reg[rs2] ? 1 : 0; //slt
+				break;
+			case 0x3:
+				reg[rd] = reg[rs1] < reg[rs2] ? 1 : 0; //sltu
+				break;
+			case 0x4:
+				reg[rd] = reg[rs1] ^ reg[rs2]; //xor
+				break;
+			case 0x5:
+				if(funt7 == 0x00)
+					reg[rd] = reg[rs1] >> reg[rs2]; //srl
+				else
+					reg[rd] = reg[rs1] >> reg[rs2]; //sra
+				break;
+			case 0x6:
+				reg[rd] = reg[rs1] | reg[rs2]; //or;
+				break;
+			case 0x7:
+				reg[rd] = reg[rs1] & reg[rs2]; //and
+			}
+			break;
+		default:
+			System.out.println("Opcode " + opcode + " not yet implemented");
+			break;
+		}
+		
+		pc++;
+		if(pc >= program.length)
+			return 1;
+		return 0;
+	}
+
+	public void loadProgram(int[] readProgram) {
+		program = readProgram;
+	}
+
+	public int[] getReg() {
+		return reg;
+	}
+}
--- src/riscv_simulator/RISCV_Simulator.java
+++ src/riscv_simulator/RISCV_Simulator.java
@@ -1,91 +1,43 @@
 package riscv_simulator;
 
 public class RISCV_Simulator {
-	static int pc;
-	static int reg[] = new int[32];
-	static int program[];
 	
 	public static void main(String[] args) {
-		pc = 0;
-		reg[0] = 0;
-		readProgram();
 		
-		for(;;) {
-			int instruction = program[pc];
-			int opcode = instruction & 0x7f;
-			int rd = (instruction >> 7) & 0x1f;
-			int funt3 = (instruction >> 12) & 0x7;
-			int rs1 = (instruction >> 15) & 0x1f;
-			int rs2 = (instruction >> 20) & 0x1f;
-			int funt7 = instruction >> 25;
-			int imm = instruction >> 20;
-			
-			switch(opcode) {
-			case 0x13:
-				reg[rd] = reg[rs1] + imm;
-				break;
-			case 0x33:
-				switch(funt3) {
-				case 0x0:
-					if(funt7 == 0x00)
-						reg[rd] = reg[rs1] + reg[rs2]; //add
-					else
-						reg[rd] = reg[rs1] - reg[rs2]; //sub
-					break;
-				case 0x1:
-					reg[rd] = reg[rs1] << reg[rs2]; //sll
-					break;
-				case 0x2:
-					reg[rd] = reg[rs1] < reg[rs2] ? 1 : 0; //slt
-					break;
-				case 0x3:
-					reg[rd] = reg[rs1] < reg[rs2] ? 1 : 0; //sltu
-					break;
-				case 0x4:
-					reg[rd] = reg[rs1] ^ reg[rs2]; //xor
-					break;
-				case 0x5:
-					if(funt7 == 0x00)
-						reg[rd] = reg[rs1] >> reg[rs2]; //srl
-					else
-						reg[rd] = reg[rs1] >> reg[rs2]; //sra
-					break;
-				case 0x6:
-					reg[rd] = reg[rs1] | reg[rs2]; //or;
-					break;
-				case 0x7:
-					reg[rd] = reg[rs1] & reg[rs2]; //and
-				}
-				break;
-			default:
-				System.out.println("Opcode " + opcode + " not yet implemented");
-				break;
-			}
-			
-			pc++;
-			printRG(); //For testing purpose
-			
-			if(pc >= program.length)
+		
+		CPU cpu1 = new CPU();
+		
+		cpu1.loadProgram(readProgram());
+		
+		for(;;)
+		{
+			int x = cpu1.oneStep();
+			printRG(cpu1); //For testing purpose
+			if(x == 1)
 				break;
 		}
-	}
+		
+		
+		
+		
+			
+		}
 
-
-	private static void printRG() {
-		for(int i = 0; i < reg.length; ++i)
+	private static void printRG(CPU cpu1) {
+		for(int i = 0; i < cpu1.getReg().length; ++i)
 		{
-			System.out.print(reg[i] + " ");
+			System.out.print(cpu1.getReg()[i] + " ");
 		}
 		System.out.println();
 	}
 	
 	
-	private static void readProgram() {
+	private static int[] readProgram() {
 		int temp[] = {
 				0x00200093, // addi x1 x0 2
 				0x00300113, // addi x2 x0 3
 				0x002081b3, // add x3 x1 x2
 		};
-		program = temp;
+		return temp;
 	}
 }
